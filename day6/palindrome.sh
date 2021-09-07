f1e899b50324fc8150ba309b993d48ef2ff8292c
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-09-07 14:05:31 +0530

    addfiles

---
+++ day6/palindrome.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+echo "enter the number"
+read num
+s=0
+rev=""
+temp=$num
+while [ $num -gt 0 ]
+do
+s=$(($num%10))
+num=$(($num/10))
+rev=$( echo ${rev}${s} )
+done
+if [ $temp -eq $rev ]
+then 
+echo "number is palindrome"
+else
+echo "number is not palindrome"
+fi
