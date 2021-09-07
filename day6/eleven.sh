e362cd39c58305aecdb6459f021d2d088924b446
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-09-07 14:03:25 +0530

    addfiles

---
+++ day6/eleven.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+Result=$((RANDOM%2))
+for ((Result=1; Result<=11; Result++))
+do
+if [[ ${Result1} -eq 0 ]]
+then 
+echo HEADS
+elif [[ ${Result1} -eq 1 ]]
+then
+echo TAILS
+fi done 
+
